5bf1572631d9c2c0d8981c594818268c166b7c43
Talal Abuzinadah <primezinadah@gmail.com>
2025-07-06 09:47:10 +0100

    Revert "Add some new keybinds"
    This reverts commit 404307bd1de263e5d0ba47de28151cf8104539f4.

--- nvim/nvim/lua/config/plugins/autotag.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"windwp/nvim-ts-autotag",
-	config = function()
-		require("nvim-ts-autotag").setup({
-			filetypes = { "html", "xml", "javascriptreact", "typescriptreact", "jsx", "tsx" },
-		})
-	end,
-}
--- nvim/nvim/lua/config/remappings.lua
+++
@@ -1,9 +0,0 @@
--- Keybinds
-vim.keymap.set("n", "<leader>ns", ":edit ~/.config/nvim/<CR>")
-vim.keymap.set("n", "<leader>nt", ":tabnew<CR>")
-vim.keymap.set("n", "<leader>ct", ":tabclose<CR>")
-vim.keymap.set("n", "<leader>w", ":w<CR>")
-vim.keymap.set("n", "<leader>q", ":q<CR>")
-vim.keymap.set("n", "<leader>wq", ":wq<CR>")
-vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("v", "jk", "<Esc>")
